bce40d370be6b19363348badc8a67958c96bc0a8
bobruis <bob.r.ruis@gmail.com>
2018-03-05 01:51:11 +0100

    Added files for challenge 1
    All files used for the challenge

---
+++ javascript/main.js
@@ -0,0 +1,4 @@
+// GLOBAL Options
+        Chart.defaults.global.defaultFontFamily = 'Lato',
+        Chart.defaults.global.defaultFontSize = 15;
+        Chart.defaults.global.defaultFontColor = '#fff';
